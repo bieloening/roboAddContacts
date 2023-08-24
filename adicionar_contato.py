8e7e2f980ff8305e7cc9dd1ae84e629c248e9e12
bieloening <oening700@gmail.com>
2023-08-24 12:00:25 -0300

    Mensagem do seu commit

--- adicionar_contato.py
+++ adicionar_contato.py
@@ -3,45 +3,23 @@ import requests
 # URL da rota para adicionar contatos (substitua pela URL correta)
 url = 'https://docs.ikatec.cloud/api/contacts'
 
-# Cabeçalhos da requisição
+# Token de autenticação
+token = 'seu_token_aqui'
+
+# Cabeçalhos da requisição, incluindo o token de autenticação
 headers = {
     'accept': 'application/json',
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {token}'  # Incluindo o token no cabeçalho
 }
 
 # Parâmetros do contato a serem enviados no corpo da requisição
 contato = {
-    "unsubscribed": False,
-    "note": "novo contato",
-    "serviceId": "12345678-abcdefghi-1234-abcdefghijkl",
-    "personId": "12345678-abcdefghi-1234-abcdefghijkl",
-    "defaultDepartmentId": "12345678-abcdefghi-1234-abcdefghijkl",
-    "defaultUserId": "12345678-abcdefghi-1234-abcdefghijkl",
-    "name": "Natã",
-    "internalName": "Natã",
-    "alternativeName": "Natã",
-    "number": "5514999999999"
+    # ... (seus parâmetros de contato aqui)
 }
 
 # Enviar a requisição POST para adicionar o novo contato
 response = requests.post(url, json=contato, headers=headers)
 
-# Verificar a resposta da requisição
-if response.status_code == 200:
-    print("Contato adicionado com sucesso!")
-    print("Resposta:")
-    print(response.json())
-else:
-    print("Erro ao adicionar contato. Código de status:", response.status_code)
-    print("Resposta:", response.text)
-
-if response.status_code == 200:
-    try:
-        print("Contato adicionado com sucesso!")
-        print("Resposta:")
-        print(response.json())
-    except requests.exceptions.JSONDecodeError:
-        print("Resposta inválida. Não foi possível decodificar como JSON.")
-else:
-    print("Erro ao adicionar contato. Código de status:", response.status_code)
-    print("Resposta:", response.text)
+# Verificar a resposta da requisição e lidar com os resultados
+# ...
